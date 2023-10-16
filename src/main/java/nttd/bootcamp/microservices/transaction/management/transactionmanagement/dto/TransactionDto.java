c5b4d9e2358dae0b52f3415890730e41bc1de0b0
jose mendoza sanchez <jmendozarv@outlook.com>
2023-10-16 15:40:12 -0500

    initial commit

--- src/main/java/nttd/bootcamp/microservices/transaction/management/transactionmanagement/dto/TransactionDto.java
+++ src/main/java/nttd/bootcamp/microservices/transaction/management/transactionmanagement/dto/TransactionDto.java
@@ -1,11 +1,8 @@
 package nttd.bootcamp.microservices.transaction.management.transactionmanagement.dto;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 @Getter
 @Setter
 @AllArgsConstructor
